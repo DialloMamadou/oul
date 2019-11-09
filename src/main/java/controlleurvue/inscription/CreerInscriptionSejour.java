852ced887377b1bc364b2259544b3d030ed5da49
malik <malik.mahamoud@outlook.fr>
2019-11-09 19:27:04 +0100

    git continuation inscription sejour

--- src/main/java/controlleurvue/inscription/CreerInscriptionSejour.java
+++ src/main/java/controlleurvue/inscription/CreerInscriptionSejour.java
@@ -1,20 +1,29 @@
 package controlleurvue.inscription;
 
 import basededonnee.DBconnexion;
+import com.jfoenix.controls.*;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import com.mysql.jdbc.PreparedStatement;
 import controlleurvue.Vue;
 import controlleurvue.centre.ConsulterCentre;
 import controlleurvue.sejour.CreerSejour;
+import dto.CentreDto;
+import dto.ClientDto;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Callback;
 import javafx.util.Duration;
+import modele.Client;
 import org.controlsfx.control.Notifications;
 import principale.Controlleur;
 
@@ -22,36 +31,149 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CreerInscriptionSejour implements Initializable, Vue {
 
 
-    public StackPane stack;
-    public TextField paiement;
-    public ComboBox client;
-    public TextField observation;
-    public DatePicker dateInscription;
-    public ComboBox sejour;
+
+
+    public JFXTreeTableView<ClientDto> clients;
+    public StackPane stackepane;
+    public JFXTextField chercheClient;
+    public JFXTextField chercheCentre;
+    public JFXTreeTableView<CentreDto> vueCentre;
+    public JFXButton valider;
+    public Pane paneclient;
+    public Label lnom;
+    public Label nom;
+    public Label lprenom;
+    public Label prenom;
+    public Label lage;
+    public Label age;
+    public Label lgroupe;
+    public Label groupe;
+    public Pane panesejour;
+    public Label lcentre;
+    public Label centre;
+    public ComboBox type;
+    public Label ltype;
+    public Label lduree;
+    public ComboBox duree;
+    public Label ldate;
+    public ComboBox date;
+    public Label ldepart;
+    public ComboBox depart;
+    public Label lprix;
+    public Label prix;
+    public Label laccompte;
+    public JFXTextField accompte;
 
     private Controlleur controlleur;
+
+
+    private void chargementClients(){
+
+    }
+
+
+
     @Override
     public void setController(Controlleur controller) {
+
+
+
+
+
         this.controlleur=controller;
 
+
+
+
+
+
+
+
+
+        JFXTreeTableColumn<ClientDto,String> duree=new JFXTreeTableColumn<>("nom");
+        duree.setPrefWidth(90);
+        duree.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ClientDto, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ClientDto, String> param) {
+                return param.getValue().getValue().nom_client;
+            }
+        });
+
+
+
+        JFXTreeTableColumn<ClientDto,String> date_debut=new JFXTreeTableColumn<>("prenom");
+        date_debut.setPrefWidth(90);
+        date_debut.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ClientDto, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ClientDto, String> param) {
+                return param.getValue().getValue().prenom_client;
+            }
+        });
+
+
+        JFXTreeTableColumn<ClientDto,String> date_fin=new JFXTreeTableColumn<>("age");
+        date_fin.setPrefWidth(90);
+        date_fin.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ClientDto, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ClientDto, String> param) {
+                return param.getValue().getValue().age_client;
+            }
+        });
+
+
+
+
+        JFXTreeTableColumn<ClientDto,String> type=new JFXTreeTableColumn<>(" groupe");
+        type.setPrefWidth(90);
+        type.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ClientDto, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ClientDto, String> param) {
+                return param.getValue().getValue().groupe;
+            }
+        });
+
+
+
+
+
+
+        ObservableList<ClientDto> rooms = FXCollections.observableArrayList();
         Connection connection= DBconnexion.getConnection();
+        String sql="SELECT * FROM client";
         try {
-            String sql="SELECT * FROM client";
             PreparedStatement ps=(PreparedStatement)connection.prepareStatement(sql);
 
             ResultSet rs=ps.executeQuery();
 
             while(rs.next()){
 
-                System.out.println("client :"+rs.getString(2)+" "+rs.getString(3));
-                this.client.getItems().add(rs.getString(2)+" "+rs.getString(3));
+
+                String sqql=             "SELECT * FROM groupe WHERE id_groupe ='" + rs.getString(5) + "'";
+
+                PreparedStatement pss=(PreparedStatement)connection.prepareStatement(sqql);
+
+                ResultSet res=pss.executeQuery();
+                String s="";
+                while(res.next()){
+
+                    s=res.getString(2);
+
+
+                }
+
+
+
+                rooms.add(new ClientDto(rs.getString(2),rs.getString(3),
+                        rs.getString(4),s));
 
             }
         } catch (SQLException ex) {
@@ -59,15 +181,111 @@ public class CreerInscriptionSejour implements Initializable, Vue {
         }
 
 
+        final TreeItem<ClientDto> root = new RecursiveTreeItem<ClientDto>(rooms, RecursiveTreeObject::getChildren);
+
+        clients.getColumns().setAll(duree,date_debut,date_fin,type);
+
+        clients.setRoot(root);
+        clients.setShowRoot(false);
+
+
+
+
+
+        this.chercheClient.textProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+                clients.setPredicate(new Predicate<TreeItem<ClientDto>>() {
+
+                    @Override
+                    public boolean test(TreeItem<ClientDto> t) {
+
+                        boolean flag = t.getValue().age_client.getValue().contains(newValue)
+                                || t.getValue().nom_client.getValue().contains(newValue)
+                                || t.getValue().prenom_client.getValue().contains(newValue)
+                                || t.getValue().groupe.getValue().contains(newValue)
+                               ;
+                        if(flag)
+                        System.out.println("trouve");
+
+                        return flag;
+
+
+                    }
+                });
+            }
+
+        });
+
+
+        clients.getSelectionModel().selectedItemProperty().addListener((Observable, oldValue, newValue)
+                ->
+                showDetails(newValue)
+        );
+
+
+
+
+
+
+
+
+        chargementCentre();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void chargementCentre() {
+
+
+
+        JFXTreeTableColumn<CentreDto,String> type=new JFXTreeTableColumn<>(" centre");
+        type.setPrefWidth(90);
+        type.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<CentreDto, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<CentreDto, String> param) {
+                return param.getValue().getValue().nom_centre;
+            }
+        });
+
+
+
+
+
+
+        ObservableList<CentreDto> rooms = FXCollections.observableArrayList();
+        Connection connection= DBconnexion.getConnection();
+        String sql="SELECT * FROM centre";
         try {
-            String sql="SELECT * FROM sejour";
             PreparedStatement ps=(PreparedStatement)connection.prepareStatement(sql);
 
             ResultSet rs=ps.executeQuery();
 
             while(rs.next()){
 
-                this.sejour.getItems().add(rs.getString(5));
+
+
+
+
+                rooms.add(new CentreDto(rs.getString(2)));
 
             }
         } catch (SQLException ex) {
@@ -75,9 +293,36 @@ public class CreerInscriptionSejour implements Initializable, Vue {
         }
 
 
+        final TreeItem<CentreDto> root = new RecursiveTreeItem<CentreDto>(rooms, RecursiveTreeObject::getChildren);
+
+        vueCentre.getColumns().setAll(type);
+
+        vueCentre.setRoot(root);
+        vueCentre.setShowRoot(false);
 
 
 
+
+
+
+
+        vueCentre.getSelectionModel().selectedItemProperty().addListener((Observable, oldValue, newValue)
+                ->
+                showCentre(newValue)
+        );
+
+
+    }
+
+    private void showCentre(TreeItem<CentreDto> newValue) {
+        centre.setText(newValue.getValue().nom_centre.getValue());
+    }
+
+    public void showDetails(TreeItem<ClientDto> pModel) {
+        nom.setText(pModel.getValue().nom_client.getValue());
+        prenom.setText(pModel.getValue().prenom_client.getValue());
+        age.setText(pModel.getValue().age_client.getValue());
+        groupe.setText(pModel.getValue().groupe.getValue());
     }
 
     public void back(MouseEvent mouseEvent) {
@@ -90,100 +335,6 @@ public class CreerInscriptionSejour implements Initializable, Vue {
 
 
 
-    public void book(MouseEvent mouseEvent) {
-
-        String sql3="INSERT INTO inscription (paiement,observation,date_inscription,code_client,id_sejour) VALUES (?,?,?,?,?)";
-
-
-
-        String sql2 = "SELECT * FROM sejour where type_sejour ='" +this.sejour.getValue().toString()+"'";
-
-
-
-        String sentence = this.client.getValue().toString();
-        String[] words = sentence.split(" ");
-
-
-        String sql = "SELECT * FROM client where nom_client ='" + words[0] + "' AND prenom_client ='"+words[1]+"'";
-
-        Connection connection = DBconnexion.getConnection();
-
-        int res=0;
-
-
-
-        try {
-
-            PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            int client=0;
-            while(resultSet.next()){
-                client=resultSet.getInt(1);
-            }
-
-
-
-
-            PreparedStatement preparedStatement1 = (PreparedStatement) connection.prepareStatement(sql2);
-            ResultSet resultSet1 = preparedStatement1.executeQuery();
-            int sejour=0;
-            while(resultSet1.next()){
-                sejour=resultSet1.getInt(1);
-            }
-
-
-
-
-
-
-            PreparedStatement ps=(PreparedStatement)connection.prepareStatement(sql3);
-            ps.setString(1,this.paiement.getText().toString());
-            ps.setString(2, this.observation.getText().toString());
-            ps.setString(3, this.dateInscription.getValue().toString());
-            ps.setInt(4, client);
-            ps.setInt(5, sejour);
-
-
-            res=ps.executeUpdate();
-
-
-
-
-        } catch (SQLException ex) {
-            Logger.getLogger(CreerSejour.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-
-        if(res>0){
-            Image image=new Image("img/mooo.png");
-            Notifications notification=Notifications.create()
-                    .title("Done")
-                    .text("INSCRIPTION creer avec succes")
-                    .hideAfter(Duration.seconds(3))
-                    .position(Pos.BOTTOM_LEFT)
-                    .graphic(new ImageView(image));
-            notification.darkStyle();
-            notification.show();
-            //   updateStatus();
-        }else{
-            Image image=new Image("img/delete.png");
-            Notifications notification=Notifications.create()
-                    .title("Error")
-                    .text("echec dans la reservation")
-                    .hideAfter(Duration.seconds(3))
-                    .position(Pos.BOTTOM_LEFT)
-                    .graphic(new ImageView(image));
-            notification.darkStyle();
-            notification.show();
-        }
-
-
-
-
-
-
-
-    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
